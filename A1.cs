8edf0e7cf77d88041138073812fbf530a21bd636
Lena <petrowa.lena2008@yandex.ru>
2024-12-05 20:36:20 +0300

    first commit

---
+++ A1.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Programm
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите свое имя")
+        string name = Console.ReadLine();
+        Console.WriteLine("Здравствуйте ," + name + "!");
+
+    }
+}
